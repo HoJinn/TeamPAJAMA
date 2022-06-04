b940ab6bc0911fb21ec7be8817798557e73212b8
HoJinChoi <quizho93@gmail.com>
2022-06-04 12:45:51 +0900

    1.1.1 배너 추가 수정

--- doctorclean/pages/about.tsx
+++ doctorclean/pages/about.tsx
@@ -48,12 +48,12 @@ const aboutContentMap = aboutContent.map((obj, i) => {
             </Typography>
           </Box>
           <Box textAlign="center" paddingTop={3}>
-            <Typography variant="body2">
+            <Typography variant="body1">
               ㈜닥터크린은 서울 및 수도권 청소와 방역,소독 서비스 전문 업체로서
               사람과 환경을 생각하며,
             </Typography>
-            <Typography variant="body2">
-              고객분들의 공간이 더 가치가 있도록 만들기 위한 최선의 노력을
+            <Typography variant="body1">
+              고객분들의 공간을 더 가치 있게 하기 위한 최선의 노력을
               제공하고 있습니다.
             </Typography>
           </Box>
